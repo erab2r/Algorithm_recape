f6c390629445d8c900fb0e626ea2f3af65dde065
erab2r <shkera213@gmail.com>
2025-09-16 17:50:01 +0600

    1

---
+++ BFS/checking_if_a_node_can_be_visited_or_not.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[1005];
+bool visited[1005];
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    visited[src] = true;
+    while(!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        for(int child : adj_list[par])
+        {
+            if(!visited[child])
+            {
+                q.push(child);
+                visited[child] = true;
+            }
+        }
+    }
+}
+int main()
+{
+    int n,e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(visited, false, sizeof(visited));
+    int src,dst;
+    cin >> src >> dst;
+    bfs(src);
+    if(visited[dst])
+    {
+        cout << "Node can be visited\n";
+    }
+    else
+    {
+        cout << "Node can't be visited\n";
+    }
+    
+}
