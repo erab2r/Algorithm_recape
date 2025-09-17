ff82de67ccc548c867dac0313a8c301eb1165ff8
erab2r <shkera213@gmail.com>
2025-09-17 21:50:16 +0600

    3

---
+++ BFS/BFS_on_2d_grid.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+char grid[1000][1000];
+int n,m;
+bool visited[1000][1000];
+vector<pair<int,int>> d = {{-1,0},{1,0},{0,-1},{0,1}};
+bool valid(int i,int j)
+{
+    if(i<0 || j<0 || i>=n || j>=m )
+        return false;
+    return true;
+}
+void bfs(int si,int sj)
+{
+  queue<pair<int,int>> q;
+  q.push({si,sj});
+  visited[si][sj] = true;
+  while (!q.empty())
+  {
+    pair<int,int> par = q.front();
+    q.pop();
+    int par_i = par.first;  //1 
+    int par_j = par.second; // 2
+    cout << par_i << " " << par_j << endl;
+    for(int i=0;i<4;i++)
+    {
+        int ci = par_i + d[i].first;
+        int cj = par_j + d[i].second;
+        if(valid(ci,cj) == true && !visited[ci][cj])
+        {
+            
+            q.push({ci,cj});
+            visited[ci][cj] = true;
+        }
+
+    }
+  }
+  
+
+}      
+int main()
+{
+    cin >> n >> m;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            cin >> grid[i][j];
+    int si,sj;
+    cin >> si >> sj;
+    memset(visited,false,sizeof(visited));
+    bfs(si,sj);
+}
+
+// complexity : 0(V+E);
