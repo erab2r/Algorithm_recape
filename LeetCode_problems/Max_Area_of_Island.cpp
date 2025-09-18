30e54c6d6b91928029a0965fcc69c151dade011d
erab2r <shkera213@gmail.com>
2025-09-19 01:19:49 +0600

    1

---
+++ LeetCode_problems/Max_Area_of_Island.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+bool visited[55][55];
+vector<pair<int,int>> d =  {{0,-1},{0,1},{-1,0},{1,0}};
+int n,m,cnt,mx;
+    bool valid(int i, int j)
+    {
+        if(i<0 || i>=n || j<0 || j>=m)
+            return false;
+        return true;
+    }
+    void dfs(int si, int sj,vector<vector<int>>& grid)
+    {
+        visited[si][sj] = true;
+        cnt++;
+        for(int i = 0;i<4;i++)
+        {
+            int ci = si + d[i].first;
+            int cj = sj + d[i].second;
+            if(valid(ci,cj) == true && !visited[ci][cj] && grid[ci][cj])
+                dfs(ci,cj,grid);
+        }
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+        mx = 0;
+        memset(visited,false,sizeof(visited));
+        for(int i = 0;i<n;i++)
+            for(int j = 0;j<m;j++)
+                if(!visited[i][j] && grid[i][j]==1)
+                {
+                    cnt = 0;
+                    dfs(i,j,grid);
+                    mx = max(cnt,mx);
+                }
+        return mx;
+    }
+};
