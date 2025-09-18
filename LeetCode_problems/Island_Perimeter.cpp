f82493186dd29425a0e3c879215b23d1609c3f51
erab2r <shkera213@gmail.com>
2025-09-18 22:57:56 +0600

    1

---
+++ LeetCode_problems/Island_Perimeter.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+int n,m;
+bool visited[105][105];
+int islandPerimeter(vector<vector<int>>& grid) {
+     n = grid.size();
+     m = grid[0].size();
+     memset(visited,false,sizeof(visited));
+     for(int i =0;i<n;i++)
+        for(int j=0;j<m;j++)
+            
+
+}
+};
